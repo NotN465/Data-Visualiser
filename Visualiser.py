573a8895aef2afc19333bc16b8ee548c86d9b207
NotN465 <eceljaK@gmail.com>
2024-07-21 01:47:17 +0200

    added a x and y value for making graphs for now

--- Visualiser.py
+++ Visualiser.py
@@ -1,28 +1,67 @@
 from tkinter import *
+
+import customtkinter
+from customtkinter import *
 from PIL import ImageTk, Image
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import matplotlib.figure
+
 class Graphs():
     def __init__(self):
-        self.x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        self.y = [5, 6, 7, 8, 2, 5, 6, 3, 7, 2]
+        self.x = []
+        self.y = []
+    def values(self):
+        user_input = entry.get()
+        try:
+            user_input = float(user_input)
+            print(f'You added {user_input} to the x value')
+            self.x.append(user_input)
+            entry.delete(0, customtkinter.END)
+            print(self.x)
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            user_input = 0
+        user_input = entry2.get()
+        try:
+            user_input = float(user_input)
+            print(f'You added {user_input} to the y value')
+            self.y.append(user_input)
+            entry2.delete(0, customtkinter.END)
+            print(self.y)
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            user_input = 0
+
+    def confirm_input(self):
+        self.plot()
     def plot(self):
         ax.clear()
         print()
         ax.plot(self.x, self.y)
         canvas.draw()
 
-root = Tk()
+root = customtkinter.CTk()
 g = Graphs()
-root.geometry("500x500")
-label = Label(root, text="Hello World")
+root.geometry("1000x1000")
+
+label = CTkLabel(root, text="Hello World")
 label.pack()
-Button(root, text="Graph", command=g.plot).pack(pady=10)
+
+CTkButton(root, text="Graph", command=g.plot).pack(pady=10)
+
+entry = CTkEntry(root, placeholder_text="Enter value x")
+entry.pack(pady=5)
+entry2 = CTkEntry(root, placeholder_text="Enter value y")
+entry2.pack(pady=5)
+
+xvalue = CTkButton(root, text="Confirm Input", command=g.values)
+xvalue.pack(pady=10)
 
 fig = matplotlib.figure.Figure()
 ax = fig.add_subplot()
+
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.get_tk_widget().pack()
 root.mainloop()
