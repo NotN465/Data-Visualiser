25fbdf9457d3b0d10025f285d92f9053dc5d9bab
NotN465 <eceljaK@gmail.com>
2024-07-21 01:10:26 +0200

    added a new class that stores all graphs

--- Visualiser.py
+++ Visualiser.py
@@ -4,25 +4,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import matplotlib.figure
-def plot():
-    ax.clear()
-    x = np.random.randint(0,10,10)
-    y = np.random.randint(0, 10, 10)
-    ax.plot(x, y)
-    canvas.draw()
+class Graphs():
+    def __init__(self):
+        self.x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.y = [5, 6, 7, 8, 2, 5, 6, 3, 7, 2]
+    def plot(self):
+        ax.clear()
+        print()
+        ax.plot(self.x, self.y)
+        canvas.draw()
 
 root = Tk()
+g = Graphs()
 root.geometry("500x500")
 label = Label(root, text="Hello World")
 label.pack()
-
-Button(root, text="Graph", command=plot).pack(pady=10)
+Button(root, text="Graph", command=g.plot).pack(pady=10)
 
 fig = matplotlib.figure.Figure()
 ax = fig.add_subplot()
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.get_tk_widget().pack()
-tooolbar = NavigationToolbar2Tk(canvas, root)
-tooolbar.update()
-tooolbar.pack(anchor="w",fill=X)
 root.mainloop()
